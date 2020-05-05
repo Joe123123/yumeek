f9b31abe2b6da04d47f00b27edf3dee95fed66d9
Zhao Wang <wangz2266@gmail.com>
2020-05-05 09:35:11 -0700

    fix warning and setup

--- client/src/components/recipe_details_page/Labels.js
+++ client/src/components/recipe_details_page/Labels.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import icons from '../../helper/heathyTags';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import icons from "../../helper/heathyTags";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,47 +10,46 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("md")]: {
       display: "flex",
       flexDirection: "column",
-      width: "30%"
+      width: "30%",
     },
     [theme.breakpoints.down("sm")]: {
-      width: "33%"
-    }
+      width: "33%",
+    },
   },
   font: {
-    fontSize: "1rem"
+    fontSize: "1rem",
   },
   label: {
     display: "flex",
-    margin: "10%"
+    margin: "10%",
   },
   icon: {
     [theme.breakpoints.down("sm")]: {
-      width: "25 em"
-    }
-  }
+      width: "25 em",
+    },
+  },
 }));
 
 export default function Labels(props) {
   const { labels } = props;
   const classes = useStyles();
-  let src = '';
+  let src = "";
 
   return (
     <section className={classes.root}>
-        {labels.map((label, index) => {
-          icons.filter(item => {
-            if (label.toLowerCase() === item.name){
-              src = item.src;
-            }
-          })
-            return (
-            <div className={classes.label} key={index}>
-              <img src={src} alt={label} className={classes.icon}/>
-              <p className={classes.font}>{label}</p>
-            </div>
-            );
+      {labels.map((label, index) => {
+        icons.forEach((item) => {
+          if (label.toLowerCase() === item.name) {
+            src = item.src;
           }
-        )}
+        });
+        return (
+          <div className={classes.label} key={index}>
+            <img src={src} alt={label} className={classes.icon} />
+            <p className={classes.font}>{label}</p>
+          </div>
+        );
+      })}
     </section>
   );
 }
--- client/src/components/recipe_details_page/SaveRecipeButton.js
+++ client/src/components/recipe_details_page/SaveRecipeButton.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import checkIfRecipeInTheList from '../../helper/checkIfRecipeInTheList';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import checkIfRecipeInTheList from "../../helper/checkIfRecipeInTheList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,71 +9,72 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "flex-end",
     marginTop: "2%",
     [theme.breakpoints.down("xs")]: {
-      display: "none"
-    }, 
+      display: "none",
+    },
   },
   style: { fontSize: 40 },
   addButton: {
     background: "#fdd770",
-    '&:hover': {
+    "&:hover": {
       background: "#f1d170",
     },
     borderRadius: 3,
     color: "#51524e",
-    borderRadius: "20px",
     border: 0,
     height: 43,
-    padding: '0 18px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .2)',
+    padding: "0 18px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .2)",
   },
   label: {
-    textTransform: 'capitalized',
+    textTransform: "capitalized",
   },
   deleteButton: {
     background: "#c9935a",
-    '&:hover': {
+    "&:hover": {
       background: "#f1d180",
     },
     borderRadius: 3,
     border: 0,
     color: "#51524e",
-    borderRadius: "20px",
     height: 43,
-    padding: '0 18px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .2)',
+    padding: "0 18px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .2)",
   },
 }));
 
-
 export default function SaveRecipeButton(props) {
-  const {recipe, savedRecipes, handleAdd, deleteRecipe } = props;
+  const { recipe, savedRecipes, handleAdd, deleteRecipe } = props;
   const classes = useStyles();
 
-  const savedRecipe = checkIfRecipeInTheList(savedRecipes, recipe.src_url, recipe.label);
+  const savedRecipe = checkIfRecipeInTheList(
+    savedRecipes,
+    recipe.src_url,
+    recipe.label
+  );
 
-  return(
-      <div className={classes.root}>
-      { savedRecipe.length > 0 ? (
-          <Button 
-            classes={{
-              root: classes.deleteButton,
-              label: classes.label, 
-            }}
-            onClick={() => deleteRecipe(savedRecipe[0])}>
-            Delete From List
-          </Button>
-        ) : ( 
-          <Button 
-            classes={{
-              root: classes.addButton,
-              label: classes.label, 
-            }} 
-            onClick={() => handleAdd(recipe)}> 
-              Add To Saved Recipes
-          </Button>
-        ) 
-     } 
-      </div>
+  return (
+    <div className={classes.root}>
+      {savedRecipe.length > 0 ? (
+        <Button
+          classes={{
+            root: classes.deleteButton,
+            label: classes.label,
+          }}
+          onClick={() => deleteRecipe(savedRecipe[0])}
+        >
+          Delete From List
+        </Button>
+      ) : (
+        <Button
+          classes={{
+            root: classes.addButton,
+            label: classes.label,
+          }}
+          onClick={() => handleAdd(recipe)}
+        >
+          Add To Saved Recipes
+        </Button>
+      )}
+    </div>
   );
 }
-
